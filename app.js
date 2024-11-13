fcce17d58c7f75f9123f758319c6e8debabc2860
Nasfurion <diego.amadio93@gmail.com>
2024-11-13 11:20:27 -0400

    Sprint 1 Completed

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import cors from 'cors'
 import productRouter from './routes/products.js'
 import userRouter from './routes/users.js'
+
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -9,8 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+app.use(cors())
+
 //API ROUTES
 app.use('/api/user', userRouter);
 app.use('/api/products', productRouter);
 
-app.listen(port)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,81 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client';
+import { hashPassword, comparePassword } from '../lib/utility.js' 
 
-const router = express.Router()
+const router = express.Router();
 
+const prisma = new PrismaClient();
 
 // SIGNUP
 router.post('/signup', async(req, res) => {
+    // Get user inputs
+    const { email, password, firstName, lastName } = req.body;
 
+    // Validate Inputs
+    if ( !email || !password || !firstName || !lastName ) {
+        return res.status(400).send('Missing required fields.'); 
+    }
+
+    // Check for existing user
+    const existingUser = await prisma.customer.findUnique({
+        where: {
+            email: email,
+        }
+    });
+    if (existingUser) {
+        return res.status(400).send('User already exists.');
+    }
+
+    // Hash Password
+    const hashedPassword = await hashPassword(password);
+
+    // Add user to DB
+    const user = await prisma.customer.create({
+        data: {
+            first_name: firstName,
+            last_name: lastName,
+            email: email,
+            password: hashedPassword
+        }
+    })
+
+    // Send Response
+    res.json({
+        'user': email,
+        'first name': firstName,
+        'last name': lastName,
+        'hashed password': hashedPassword
+    })
 })
 
 // LOGIN
 router.post('/login', async(req, res) => {
-    
+    // Get user inputs
+    const { email, password} = req.body;
+
+    // Validate Inputs
+    if ( !email || !password ) {
+        return res.status(400).send('Missing required fields.');
+    }
+
+    // Find user in DB
+    const existingUser = await prisma.customer.findUnique({
+        where: {
+            email: email,
+        }
+    });
+    if (!existingUser) {
+        return res.status(404).send('User not found');
+    }
+
+    // Verify password
+    const passwordMatch = await comparePassword(password, existingUser.password);
+    if (!passwordMatch){
+        return res.status(410).send('Invalid password')
+    }
+
+    // Send response
+    res.json({'user': email})
 })
 
 // LOGOUT
